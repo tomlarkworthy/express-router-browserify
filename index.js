c479a33bda22b8758e5b399d360beb7aff2c90e7
Tom Larkworthy <tom.larkworthy.external@zalando.de>
2020-11-23 23:08:39 +0100

    1st

---
+++ index.js
@@ -0,0 +1,6 @@
+// Expose the Router constructor
+module.exports = require('router');
+
+// Expose request and response
+module.exports.Request = require('./request');
+module.exports.Response = require('./response');
